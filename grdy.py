fb6905328f9c4fe673e317e8ab84d6f68d488d58
Jingchi Zhang <Kingdomyi@Jingchis-MacBook-Pro.local>
2017-05-11 04:05:52 -0400

    start greedy

---
+++ grdy.py
@@ -0,0 +1,30 @@
+import re,sys
+
+
+
+
+
+
+
+def load_dic(filename):
+	with open(filename) as f:
+		lines = f.readlines()
+
+	pass
+
+def load_gra(filename):
+	with open(filename) as f:
+		lines = f.readlines()
+
+	pass
+
+def load_text(filename):
+	with open(filename) as f:
+		lines = f.readlines()
+	pass
+
+
+
+if __name__ == '__main__':
+
+	args = sys.argv[1:]
