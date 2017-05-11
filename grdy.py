1b21d424843b8ddfae9f6abd75346e6e38eac607
Jingchi Zhang <Kingdomyi@Jingchis-MacBook-Pro.local>
2017-05-11 05:01:38 -0400

    init set up

--- grdy.py
+++ grdy.py
@@ -1,8 +1,8 @@
 import re,sys
 
-
-
-
+gra_list = []
+t_list = []
+dic = {}
 
 
 
@@ -10,21 +10,41 @@ def load_dic(filename):
 	with open(filename) as f:
 		lines = f.readlines()
 
-	pass
+	for line in lines:
+		if line[0] not in dic:
+			dic[line[0]] = [line[1]]
+		else:
+			dic[line[0]].append(line[1])
 
 def load_gra(filename):
 	with open(filename) as f:
 		lines = f.readlines()
 
-	pass
+	for line in lines:
+		pair = [line[0],line[1]]
+
+		gra_list.append(pair)
 
 def load_text(filename):
 	with open(filename) as f:
 		lines = f.readlines()
-	pass
+	for l in range(0:len(lines)):
+		pair = [lines[l],lines[l+1]]
+
+		t_list.append(pair)
 
 
 
 if __name__ == '__main__':
-
+	#dic gra text
 	args = sys.argv[1:]
+	if len(args) != 3:
+		print "use default"
+		load_dic("dict.txt")
+		load_text("text.txt")
+		load_gra("gram.txt")
+	else:
+		load_dic(args[0])
+		load_gra(args[1])
+		load_text(args[2])
+	
